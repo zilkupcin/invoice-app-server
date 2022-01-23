8d66a6c6f704a7c57dedb6f413797309ceddd4a8
Grab a Treat <grabatreatapp@gmail.com>
2022-01-23 20:18:18 +0000

    Add validation

---
+++ validation.js
@@ -0,0 +1,25 @@
+const Joi = require("joi");
+const User = require("./models/user.model");
+
+// Register Validation
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().min(6).required(),
+    password: Joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+// Login Validation
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().min(6).required(),
+    password: Joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
