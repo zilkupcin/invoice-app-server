25f2149e2838df96e49391f998aaf60a9d2fa298
Grab a Treat <grabatreatapp@gmail.com>
2022-01-30 19:59:03 +0000

    Add some comments

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,12 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const axios = require("axios");
-var mongoose = require("mongoose");
-const invoices = require("../data/data.json");
 const User = require("../models/user.model");
-const Invoice = require("../models/invoice.model");
 const { registerValidation, loginValidation } = require("../validation");
 
-// Validation
-
 router.post("/register", async (req, res) => {
+  // Validate user input
   const { error } = registerValidation(req.body);
-
   if (error) res.status(400).send(error.details[0].message);
 
   // Check if the user is already in the database
@@ -23,12 +17,14 @@ router.post("/register", async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
+  // Create a new User entry
   const user = new User({
     username: req.body.username,
     password: hashedPassword,
   });
 
   try {
+    // Save the new User
     const savedUser = await user.save();
     res.send({ user: savedUser._id });
   } catch (e) {
@@ -37,8 +33,8 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
+  // Validate user input
   const { error } = loginValidation(req.body);
-
   if (error) res.status(400).send(error.details[0].message);
 
   // Check if the username exists
--- routes/invoices.js
+++ routes/invoices.js
@@ -6,8 +6,10 @@ const Invoice = require("../models/invoice.model");
 const { invoiceValidation } = require("../validation");
 
 router.get("/", verify, async (req, res) => {
+  // Find all invoices for this specific user
   const userInvoices = await Invoice.find({ userId: req.user._id });
 
+  // If there are invoices, return them to the client
   if (userInvoices) {
     res.json(userInvoices);
   } else {
@@ -19,11 +21,13 @@ router.get("/:invoiceId", verify, async (req, res) => {
   const invoiceId = req.params.invoiceId;
 
   try {
+    // Find the specific invoice
     const invoice = await Invoice.findOne({
       userId: req.user._id,
       _id: invoiceId,
     });
 
+    // Return the invoices in JSON format
     if (invoice) {
       res.json(invoice);
     } else {
@@ -38,6 +42,7 @@ router.post("/delete/:invoiceId", verify, async (req, res) => {
   const invoiceId = req.params.invoiceId;
 
   try {
+    // Find and delete an invoice with the given ID
     const response = await Invoice.deleteOne({
       userId: req.user._id,
       _id: invoiceId,
@@ -56,12 +61,13 @@ router.post("/delete/:invoiceId", verify, async (req, res) => {
 router.post("/edit/:invoiceId", verify, async (req, res) => {
   const invoiceId = req.params.invoiceId;
 
-  // Validate data
+  // Validate user input
   const { error } = invoiceValidation(req.body);
 
   if (error) res.status(400).json(error);
 
   try {
+    // Update the invoice with given data
     const invoice = await Invoice.updateOne(
       {
         _id: invoiceId,
@@ -80,6 +86,7 @@ router.get("/paid/:invoiceId", verify, async (req, res) => {
   const invoiceId = req.params.invoiceId;
 
   try {
+    // Set the specific invoice as paid
     const invoice = await Invoice.updateOne(
       {
         _id: invoiceId,
@@ -99,16 +106,19 @@ router.post("/add", verify, async (req, res) => {
 
   const invoiceData = { ...req.body };
 
-  // Validate data
+  // Validate the user input
   const { error } = invoiceValidation(invoiceData);
   if (error) res.status(400).send(error.details[0].message);
 
+  // Validate each invoice item
   invoiceData.items.forEach(
     (item) => (item._id = new mongoose.mongo.ObjectId())
   );
 
+  // Create a new Invoice
   const invoice = new Invoice({ ...invoiceData, userId });
 
+  // Sasve the invoice
   const savedInvoice = await invoice.save();
   res.status(200).send(savedInvoice._id);
 });
